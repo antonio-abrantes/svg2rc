bf3802132d79bb56ae464f4d533d8834b574946b
antonio-abrantes <antonio-abrantes@hotmail.com.br>
2026-07-17 14:25:15 -0300

    Add resizable panels, i18n persistence, and project branding polish

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,8 +5,14 @@ import { SvgInputPanel } from "@/components/SvgInputPanel";
 import { CodeOutputPanel } from "@/components/CodeOutputPanel";
 import { ConvertButton } from "@/components/ConvertButton";
 import { InstallShortcutDialog } from "@/components/InstallShortcutDialog";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 import { useSvgConverter } from "@/hooks/useSvgConverter";
 import { useInstallShortcut } from "@/hooks/useInstallShortcut";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 export const Route = createFileRoute("/")({
   head: () => ({
@@ -50,49 +56,68 @@ function ConverterApp() {
     applyConversion,
   } = useSvgConverter();
   const shortcut = useInstallShortcut();
+  const isMobile = useIsMobile();
+
+  const inputPanel = (
+    <SvgInputPanel
+      value={input}
+      error={error}
+      onChange={(v) => setInput(v)}
+      onPaste={(v) => setInput(v, { isPaste: true })}
+    />
+  );
+
+  const outputPanel = (
+    <CodeOutputPanel
+      value={output}
+      format={format}
+      componentName={componentName}
+      nameRaw={nameRaw}
+      onNameChange={setNameRaw}
+      hasError={!!error}
+    />
+  );
 
   return (
     <div className="flex h-screen flex-col bg-background">
       <Header format={format} onFormatChange={setFormat} />
-      <main className="flex min-h-0 flex-1 flex-col md:flex-row md:relative">
-        <div className="flex min-h-0 flex-1 border-b border-border md:border-b-0 md:border-r">
-          <SvgInputPanel
-            value={input}
-            error={error}
-            onChange={(v) => setInput(v)}
-            onPaste={(v) => setInput(v, { isPaste: true })}
-          />
-        </div>
 
-        {/* Mobile bar */}
-        <div className="md:hidden">
+      {isMobile ? (
+        <main className="flex min-h-0 flex-1 flex-col">
+          <div className="flex min-h-0 flex-1 border-b border-border">{inputPanel}</div>
           <ConvertButton
             onClick={applyConversion}
             disabled={!isDirty}
             variant="bar"
           />
-        </div>
+          <div className="flex min-h-0 flex-1">{outputPanel}</div>
+        </main>
+      ) : (
+        <main className="min-h-0 flex-1">
+          <ResizablePanelGroup orientation="horizontal" className="h-full">
+            <ResizablePanel defaultSize="50" minSize={100} className="min-w-0">
+              <div className="flex h-full min-h-0 min-w-0">{inputPanel}</div>
+            </ResizablePanel>
 
-        {/* Desktop floating button */}
-        <div className="hidden md:block">
-          <ConvertButton
-            onClick={applyConversion}
-            disabled={!isDirty}
-            variant="floating"
-          />
-        </div>
+            <ResizableHandle>
+              <div
+                className="pointer-events-auto absolute left-1/2 top-1/2 z-20 -translate-x-1/2 -translate-y-1/2"
+                onPointerDown={(e) => e.stopPropagation()}
+              >
+                <ConvertButton
+                  onClick={applyConversion}
+                  disabled={!isDirty}
+                  variant="floating"
+                />
+              </div>
+            </ResizableHandle>
 
-        <div className="flex min-h-0 flex-1">
-          <CodeOutputPanel
-            value={output}
-            format={format}
-            componentName={componentName}
-            nameRaw={nameRaw}
-            onNameChange={setNameRaw}
-            hasError={!!error}
-          />
-        </div>
-      </main>
+            <ResizablePanel defaultSize="50" minSize={100} className="min-w-0">
+              <div className="flex h-full min-h-0 min-w-0">{outputPanel}</div>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </main>
+      )}
 
       <InstallShortcutDialog
         open={shortcut.open}
